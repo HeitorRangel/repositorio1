8dc8659c28bc42568d1bf09066be8fde561ccf80
HeitorRangel <87882995+HeitorRangel@users.noreply.github.com>
2021-08-02 13:03:36 -0300

    Update alterarVeiculo.php

--- alterarVeiculo.php
+++ alterarVeiculo.php
@@ -6,25 +6,24 @@
         ID: <input type=text name=idProprietario>
         Nova forma de pagamento: <input type=text name=formaPagamento>
         <input type=submit value="Alterar forma de pagamento">
-        <a href="Pagina2- Formulário.php"><input type=button value="Voltar"></a>
+        <a href="indexFormulário.php"><input type=button value="Voltar"></a>
     </form>
 </html>
 
 <?php
-    if(isset($_POST["idProprietario"]))
-    {
-        $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
-        mysqli_select_db($conexao,"veiculos.estacionamento");
-        $idProprietario= $_POST["idProprietario"];
-        $formaPagamento= $_POST["formaPagamento"];
-        mysqli_query ($conexao,"update veiculos set formaPagamento='$formaPagamento' where idProprietario=$idProprietario");
-        $consulta = mysqli_query($conexao,"select * from veiculos");
+   
+   $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
+    mysqli_select_db($conexao,"estacionamento");
+    $idProprietario= $_POST["idProprietario"];
+    $formaPagamento= $_POST["formaPagamento"];
+    mysqli_query ($conexao,"update veiculos set formaPagamento='$formaPagamento' where idProprietario=$idProprietario");
+    $consulta = mysqli_query($conexao,"select * from veiculos");
 
-        while($exibir = mysqli_fetch_array($consulta))
-        {
-            echo "<br>ID: ".$exibir['idProprietario'];
-            echo " - Forma de Pagamento: ".$exibir['formaPagamento'];
-        }
-        mysqli_close($conexao);
+    while($exibir = mysqli_fetch_array($consulta))
+    {
+        echo "<br>ID: ".$exibir['idProprietario'];
+        echo " - Forma de Pagamento: ".$exibir['formaPagamento'];
     }
+    mysqli_close($conexao);
+    
 ?>
