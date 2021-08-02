f6da7cc7d0dc8f67a0a5582268edb88152003b92
HeitorRangel <87882995+HeitorRangel@users.noreply.github.com>
2021-08-02 13:57:54 -0300

    Update alterarVeiculo.php

--- alterarVeiculo.php
+++ alterarVeiculo.php
@@ -3,7 +3,7 @@
     Para alterar a forma de pagamento clique abaixo: <hr>
 
     <form method=POST>
-        ID: <input type=text name=idProprietario>
+        Placa: <input type=text name=placaVeiculo>
         Nova forma de pagamento: <input type=text name=formaPagamento>
         <input type=submit value="Alterar forma de pagamento">
         <a href="indexFormulário.php"><input type=button value="Voltar"></a>
@@ -14,16 +14,10 @@
    
    $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
     mysqli_select_db($conexao,"estacionamento");
-    $idProprietario= $_POST["idProprietario"];
+    $placaVeiculo= $_POST["placaVeiculo"];
     $formaPagamento= $_POST["formaPagamento"];
-    mysqli_query ($conexao,"update veiculos set formaPagamento='$formaPagamento' where idProprietario=$idProprietario");
+    mysqli_query ($conexao,"update veiculos set formaPagamento='$formaPagamento' where placaVeiculo=$placaVeiculo");
     $consulta = mysqli_query($conexao,"select * from veiculos");
-
-    while($exibir = mysqli_fetch_array($consulta))
-    {
-        echo "<br>ID: ".$exibir['idProprietario'];
-        echo " - Forma de Pagamento: ".$exibir['formaPagamento'];
-    }
     mysqli_close($conexao);
     
 ?>
