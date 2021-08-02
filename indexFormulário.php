ada6f51743a7db89150bbc6383a95039325ec850
HeitorRangel <87882995+HeitorRangel@users.noreply.github.com>
2021-08-02 13:55:57 -0300

    Update indexFormulário.php

--- indexFormulário.php
+++ indexFormulário.php
@@ -1,54 +1,55 @@
 <!DOCTYPE html>
 <html>
-    <title>Pagina 2-Formulário</title>
+    <title>Página 2- Formulário</title>
     <head>
-        <meta charset="utf-8">        
+        <meta charset="utf-8">    
+        <link rel="stylesheet" href="style2.css">    
     </head>
     <body>
+        <div id="conteiner">
         
-        <h1>Preencha o formulário com seus Dados Pessoais</h1>
+        <h1  id="cabecalho">Preencha o formulário com seus Dados Pessoais</h1>
         <hr> 
-        <form method=POST>
-        
-            <br><b>Digite seu ID</b>
-            <input required type=text name=idProprietario><br>
-            <br><b>Digite seu nome:</b>
-            <input required type=text name=nomeProprietario><br>
-            <br><b>Digite seu CPF</b>
-            <input required type=text name=cpfProprietario><br>
-        
-            <br><b>Veículo:</b><select name=tipoVeiculo>
-                <option value=bicicleta>Bicicleta
-                <option value=moto>Moto
-                <option value=carro>Carro
-                <option value=reboque>Reboque
-            </select><br>
+            <form method=POST>
+            
+                <br><b>Placa</b>
+                <input required type=text name=placaVeiculo><br>
+                <br><b>Nome:</b>
+                <input required type=text name=nomeProprietario><br>
+                <br><b>CPF</b>
+                <input required type=text name=cpfProprietario><br>
+            
+                <br><b>Veículo: </b><select name=tipoVeiculo>
+                    <option value=moto>Moto
+                    <option value=carro>Carro
+                    <option value=reboque>Reboque
+                </select><br><br>
 
-            <h3>Data e Horário de Check-In</h3><br>
-            <input type="datetime-local" name = "dataHoraCheckIn"><br>
-            <h3>Data e Horário de Check-Out</h3><br>
-            <input type="datetime-local" name = "dataHoraCheckOut"><br>
-            
-            
-            <br><b>Forma de formaPagamento:</b>
-            <input type=radio name=formaPagamento value="cartao de credito">Cartão de Crédito
-            <input type=radio name=formaPagamento value=dinheiro>Dinheiro 
-            <input type=radio name=formaPagamento value=boleto>Boleto Bancário<br>
-            <hr>
-            
-            <br>
-            <h3>Você concorda com as restrições  do Estacionamento?</h3>
-            <br><input required type=radio name=restricao value=concorda>Sim, eu concordo.
-            <br><input required type=radio name=restricao value="nao concorda">Não concordo.
+                <h3>Data e Horário de Check-In</h3><br>
+                <input type="datetime-local" name = "dataHoraCheckIn"><br>
+                <h3><br>Data e Horário de Check-Out</h3><br>
+                <input type="datetime-local" name = "dataHoraCheckOut"><br>
+                
+                
+                <br><b>Forma de Pagamento:</b><br>
+                <input type=radio name=formaPagamento value="cartao de credito">Cartão de Crédito<br>
+                <input type=radio name=formaPagamento value=dinheiro>Dinheiro<br>
+                <input type=radio name=formaPagamento value=boleto>Boleto Bancário<br><br>
+                <hr>
+                
+                <br>
+                <h3>Você concorda com as restrições  do Estacionamento?</h3>
+                <br><input required type=radio name=restricao value=concorda>Sim, eu concordo.
 
-            <br><center>
-            <br><input type=submit value=Enviar>
-            <input type=reset value=Apagar>
-            <a href="apagarVeiculo-Codigo.php"><input type=button value="Apagar Veiculo"></a>
-            <a href="alterarVeiculo.php"><input type=button value="Alterar Veiculo"></a>
-            </center>
-        
-        </form>
+                <br><center>
+                <br><input type=submit value=Enviar>
+                <input type=reset value=Apagar>
+                <a href="apagarVeiculo-Codigo.php"><input type=button value="Apagar Veiculo"></a>
+                <a href="alterarVeiculo.php"><input type=button value="Alterar Veiculo"></a>
+                </center>
+            
+            </form>
+        </div>
     </body>
 </html>
 
@@ -56,7 +57,7 @@
     $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
 
     mysqli_select_db($conexao,"estacionamento");
-    $idProprietario = $_POST["idProprietario"];
+    $placaVeiculo = $_POST["placaVeiculo"];
     $nomeProprietario = $_POST["nomeProprietario"];
     $cpfProprietario = $_POST["cpfProprietario"];
     $tipoVeiculo = $_POST["tipoVeiculo"];
@@ -65,19 +66,7 @@
     $formaPagamento = $_POST["formaPagamento"];
     $restricao = $_POST["restricao"];
 
-    mysqli_query($conexao, "insert into veiculos values('$idProprietario', '$nomeProprietario', '$cpfProprietario', '$tipoVeiculo', '$dataHoraCheckIn', '$dataHoraCheckOut', '$formaPagamento', '$restricao')");
+    mysqli_query($conexao, "insert into veiculos values('$placaVeiculo', '$nomeProprietario', '$cpfProprietario', '$tipoVeiculo', '$dataHoraCheckIn', '$dataHoraCheckOut', '$formaPagamento', '$restricao')");
     $consulta = mysqli_query($conexao,"select * from veiculos");
-    while($exibir = mysqli_fetch_array($consulta))
-    {
-        echo "<br>ID:".$exibir['idProprietario'];
-        echo " - Nome:".$exibir['nomeProprietario'];
-        echo " - CPF:".$exibir['cpfProprietario'];
-        echo " - Veiculo:".$exibir['tipoVeiculo'];
-        echo " - Data e Hora de CheckIn:".$exibir['dataHoraCheckIn'];
-        echo " - Data e Hora de CheckOut:".$exibir['dataHoraCheckOut'];
-        echo " - Pagamento:".$exibir['formaPagamento'];
-        echo " - Concordância com Restrições:".$exibir['restricao'];
-    }
-
     mysqli_close($conexao);
 ?>
