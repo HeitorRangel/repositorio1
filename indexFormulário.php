643e2b01f2165c3faca6197a6e19274a25547a96
HeitorRangel <87882995+HeitorRangel@users.noreply.github.com>
2021-08-02 12:50:49 -0300

    Update indexFormulário.php

--- indexFormulário.php
+++ indexFormulário.php
@@ -11,7 +11,7 @@
         <form method=POST>
         
             <br><b>Digite seu ID</b>
-            <input required type=text name=id><br>
+            <input required type=text name=idProprietario><br>
             <br><b>Digite seu nome:</b>
             <input required type=text name=nomeProprietario><br>
             <br><b>Digite seu CPF</b>
@@ -53,35 +53,31 @@
 </html>
 
 <?php
-    if(isset($_POST["idProprietario"]))
+    $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
+
+    mysqli_select_db($conexao,"estacionamento");
+    $idProprietario = $_POST["idProprietario"];
+    $nomeProprietario = $_POST["nomeProprietario"];
+    $cpfProprietario = $_POST["cpfProprietario"];
+    $tipoVeiculo = $_POST["tipoVeiculo"];
+    $dataHoraCheckIn = $_POST["dataHoraCheckIn"];
+    $dataHoraCheckOut = $_POST["dataHoraCheckOut"];
+    $formaPagamento = $_POST["formaPagamento"];
+    $restricao = $_POST["restricao"];
+
+    mysqli_query($conexao, "insert into veiculos values('$idProprietario', '$nomeProprietario', '$cpfProprietario', '$tipoVeiculo', '$dataHoraCheckIn', '$dataHoraCheckOut', '$formaPagamento', '$restricao')");
+    $consulta = mysqli_query($conexao,"select * from veiculos");
+    while($exibir = mysqli_fetch_array($consulta))
     {
-        echo "entrou no if";
-        $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
-
-        mysqli_select_db($conexao,"estacionamento");
-        $idProprietario = $_POST["idProprietario"];
-        $nomeProprietario = $_POST["nomeProprietario"];
-        $cpfProprietario = $_POST["cpfProprietario"];
-        $tipoVeiculo = $_POST["tipoVeiculo"];
-        $dataHoraCheckIn = $_POST["dataHoraCheckIn"];
-        $dataHoraCheckOut = $_POST["dataHoraCheckOut"];
-        $formaPagamento = $_POST["formaPagamento"];
-        $restricao = $_POST["restricao"];
-
-        mysqli_query($conexao, "insert into veiculos values('$idProprietario', '$nomeProprietario', '$cpfProprietario', '$tipoVeiculo', '$dataHoraCheckIn', '$dataHoraCheckOut', '$formaPagamento', '$restricao')");
-        $consulta = mysqli_query($conexao,"select * from veiculos");
-        while($exibir = mysqli_fetch_array($consulta))
-        {
-            echo "<br>ID:".$exibir['idProprietario'];
-            echo " - Nome:".$exibir['nomeProprietario'];
-            echo " - CPF:".$exibir['cpfProprietario'];
-            echo " - Veiculo:".$exibir['tipoVeiculo'];
-            echo " - Data e Hora de CheckIn:".$exibir['dataHoraCheckIn'];
-            echo " - Data e Hora de CheckOut:".$exibir['dataHoraCheckOut'];
-            echo " - Pagamento:".$exibir['formaPagamento'];
-            echo " - Concordância com Restrições:".$exibir['restricao'];
-        }
-
-        mysqli_close($conexao);
+        echo "<br>ID:".$exibir['idProprietario'];
+        echo " - Nome:".$exibir['nomeProprietario'];
+        echo " - CPF:".$exibir['cpfProprietario'];
+        echo " - Veiculo:".$exibir['tipoVeiculo'];
+        echo " - Data e Hora de CheckIn:".$exibir['dataHoraCheckIn'];
+        echo " - Data e Hora de CheckOut:".$exibir['dataHoraCheckOut'];
+        echo " - Pagamento:".$exibir['formaPagamento'];
+        echo " - Concordância com Restrições:".$exibir['restricao'];
     }
+
+    mysqli_close($conexao);
 ?>
