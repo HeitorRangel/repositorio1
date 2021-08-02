1cfeec1f2f73f5854fbd3e7cb27c0b268c0b5b2f
HeitorRangel <87882995+HeitorRangel@users.noreply.github.com>
2021-08-02 13:56:57 -0300

    Update apagarVeiculo-Codigo.php

--- apagarVeiculo-Codigo.php
+++ apagarVeiculo-Codigo.php
@@ -2,7 +2,7 @@
     <meta charset=UTF-8>
     Para apagar o veiculo clique abaixo: <hr>
     <form method=POST>
-        Id: <input type=text name=idProprietario>
+        Placa: <input type=text name=placaVeiculo>
         <input type=submit value="Apagar Veiculo">
         <a href="indexFormulário.php"><input type=button value="Voltar"></a>
     </form>
@@ -13,23 +13,10 @@
     $conexao=mysqli_connect("database1.cw3frybg0axw.us-east-1.rds.amazonaws.com", "admin", "HECR12-34h");
 
     mysqli_select_db($conexao,"estacionamento");
-    $idProprietario = $_POST["idProprietario"];
+    $placaVeiculo = $_POST["placaVeiculo"];
 
-    mysqli_query($conexao,"delete from veiculos where idProprietario='$idProprietario'");
+    mysqli_query($conexao,"delete from veiculos where placaVeiculo='$placaVeiculo'");
     $consulta = mysqli_query($conexao,"select * from veiculos");
-
-    while($exibir = mysqli_fetch_array($consulta))
-    {
-        echo "<br>ID:".$exibir['idProprietario'];
-        echo " - Nome:".$exibir['nomeProprietario'];
-        echo " - CPF:".$exibir['cpfProprietario'];
-        echo " - Veiculo:".$exibir['tipoVeiculo'];
-        echo " - Data e Hora de CheckIn:".$exibir['dataHoraCheckIn'];
-        echo " - Data e Hora de CheckOut:".$exibir['dataHoraCheckOut'];
-        echo " - Pagamento:".$exibir['formaPagamento'];
-        echo " - Concordância com Restrições:".$exibir['restricao'];
-        echo " - Termo de Responsabilidade:".$exibir['termoResponsabilidade'];
-    }
     mysqli_close($conexao);
     
 ?>
